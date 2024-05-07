642f75a72d35e92f0a3c01bea5df28cf461ad374
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-05-07 14:09:23 -0700

    initial commit
    set up the homepage and the clerk auth route to protect that page. cleaned out the default nextjs code

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isProtectedRoute = createRouteMatcher([
+    '/', //this protects the root route
+]);
+
+export default clerkMiddleware((auth, req) => {
+    if (isProtectedRoute(req)) auth().protect(); //if the route req is protected, protect it using auth().protect()
+    return NextResponse.next(); //otherwise return the next response
+});
+
+//catches every route except for the static assets
+export const config = {
+    matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
