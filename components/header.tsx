713b8d2dbdc52213512a3b4fc1b06453b8375add
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-05-08 22:41:45 -0700

    navigation
    the navigation component contains the navitems that render different labels and if the page is the current path it remains active

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,6 @@
 import HeaderLogo from "@/components/header-logo";
+import Navigation from "@/components/navigation";
+
 
 export const Header = () => {
     return (
@@ -8,6 +10,7 @@ export const Header = () => {
                 <div className='w-full flex items-center justify-between mb-14'>
                     <div className='flex items-center lg:gap-x-16'>
                         <HeaderLogo />
+                        <Navigation />
                     </div>
                 </div>
             </div>
