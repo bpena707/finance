bec8632c516ce8cf9b0cf297c22488201a1022f7
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-05-08 20:35:13 -0700

    api route
    testing the api route to make sure it works correctly incliding the clerk middleware

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,17 +2,34 @@
 
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import { z } from 'zod'
+import { zValidator } from "@hono/zod-validator";
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
 //defines get route and returns a json response
-app.get('/hello', (c) => {
-    return c.json({
-        message: 'Hello Next.js!',
+app
+    .get(
+        '/hello',
+        clerkMiddleware(),
+        (c) => {
+            const auth = getAuth(c)
+            if (!auth?.userId) {
+                return c.json({
+                    error: 'unauthenticated',
+                })
+
+            }
+
+        return c.json({
+            message: 'Hello Next.js!',
+            uerId: auth.userId,
+        })
     })
-})
+
 
 //allows hono to use the route handlers
 export const GET = handle(app)
