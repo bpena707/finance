bdebbb3823cf53460315cdabe06a3c39c86f20c5
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-05-13 17:23:03 -0700

    styling
    more styling applied to account page to render card over the header

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -16,7 +16,7 @@ import {useNewAccount} from "@/features/hooks/use-new-account";
 const AccountsPage = () => {
     const newAccount = useNewAccount()
   return (
-      <div>
+      <div className='max-w-2xl mx-auto w-full pb-10 -mt-24'>
           <Card className='border-none drop-shadow-sm'>
               <CardHeader className='gap-y-2 lg:flex-row lg:justify-between'>
                   <CardTitle className='text-xl line-clamp-1'>
