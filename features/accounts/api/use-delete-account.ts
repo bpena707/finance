524279ba9e0b84216dd0875c89d4178a57a5628b
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-05-24 23:50:03 -0700

    Create use-delete-account.ts
    the api hook uses mutation function to mutate the values and delete based on id and only passing the response type.

---
+++ features/accounts/api/use-delete-account.ts
@@ -0,0 +1,38 @@
+import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { client } from "@/lib/hono";
+import {toast} from "sonner";
+
+// only the ResponseType is needed to delete
+type ResponseType = InferResponseType<typeof client.api.accounts[":id"]["$delete"]>;
+
+export const useDeleteAccount = (id: string) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<
+      ResponseType,
+      Error
+  >({
+      mutationFn: async () => {
+          //only relying on the param object to pass the idand delete the account
+          const response = await client.api.accounts[":id"]["$delete"]({
+                param: { id },
+          })
+          return await response.json();
+
+      },
+      onSuccess: () => {
+          //refetch all accounts after creating a new account
+          toast.success("Account deleted successfully");
+          //primary update account with id as key and all accounts
+          queryClient.invalidateQueries({ queryKey: ["accounts", { id }] });
+            queryClient.invalidateQueries({ queryKey: ["accounts"] });
+      },
+      onError: () => {
+          toast.error("Failed to delete account");
+      }
+  });
+
+  return mutation;
+};
